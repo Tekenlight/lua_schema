be44fb76a8bfab02197705c7bc0fd047d72f0e8b
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2021-04-23 11:26:19 +0530

    .

--- from_xml/basic_string_test.lua
+++ from_xml/basic_string_test.lua
@@ -1,11 +1,11 @@
 #!/opt/local/bin/lua
 
 local xml_string = [=[<?xml version="1.0" encoding="UTF-8"?>
-<ns1:basic_string xmlns:ns1="http://test_example.prototype">hello hello</ns1:basic_string>]=]
+<ns1:basic_string xmlns:ns1="http://example.prototype">hello hello</ns1:basic_string>]=]
 
 mhf = require("message_handler_factory")
 
-basic_string = mhf:get_message_handler("basic_string", "http://test_example.prototype");
+basic_string = mhf:get_message_handler("basic_string", "http://example.prototype");
 
 local content, msg = basic_string:from_xml(xml_string)
 if (type(content) == 'table') then require 'pl.pretty'.dump(content);
