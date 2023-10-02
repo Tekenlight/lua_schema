cd9976eb6ced9c75c34b7fcec349a5e09c3c97d8
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2023-10-02 05:53:59 +0000

    Edit

--- test/gfrom_xml/test-date.lua
+++ test/gfrom_xml/test-date.lua
@@ -1,4 +1,5 @@
 local dt = require('lua_schema.date_utils');
+print("==================================== UTC ========================================");
 local a = dt.now(true);
 
 --local b = dt.subtract_duration_from_date(a, "1|1|3600");
@@ -18,5 +19,23 @@ local r = dt.add_duration_to_date(b, d);
 print(debug.getinfo(1).source, debug.getinfo(1).currentline, r);
 
 
+print("==================================== LOCAL ========================================");
 
+local a = dt.now(false);
+
+--local b = dt.subtract_duration_from_date(a, "1|1|3600");
+local b = dt.add_duration_to_date(a, "1|1|3600");
+--local b = dt.subtract_duration_from_date(a, "0|0|3600");
+
+print(debug.getinfo(1).source, debug.getinfo(1).currentline, a);
+print(debug.getinfo(1).source, debug.getinfo(1).currentline, b);
+
+local d = dt.date_diff(a, b);
+print(debug.getinfo(1).source, debug.getinfo(1).currentline);
+require 'pl.pretty'.dump(d);
+print(debug.getinfo(1).source, debug.getinfo(1).currentline);
+
+
+local r = dt.add_duration_to_date(b, d);
+print(debug.getinfo(1).source, debug.getinfo(1).currentline, r);
 
